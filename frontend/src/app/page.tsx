a6844ae764471bdb8f792c4da7303e8fbdcff9a8
taafyy <taffy.hafeez@gmail.com>
2025-02-23 15:43:43 -0500

    mobile home fix?

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -9,13 +9,15 @@ const Home = () => {
       <main className="min-h-screen">
         <div className="flex flex-col items-center mt-40">
           <WelcomeTitle />
-          <Button
-            className={`${pressStart.className} opacity-0 animate-fade-in-delay text-foreground text-[10px] font-bold 
-            hover:text-maize transition-colors duration-300`}
-            variant="link"
-          >
-            <Link href="/docs">Get Started</Link>
-          </Button>
+          <Link href="/docs" passHref prefetch={false}>
+            <Button
+              className={`${pressStart.className} opacity-0 animate-fade-in-delay text-foreground text-[10px] font-bold 
+    hover:text-maize transition-colors duration-300`}
+              variant="link"
+            >
+              Get Started
+            </Button>
+          </Link>
         </div>
       </main>
     </div>
